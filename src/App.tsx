4443c16ae576eda53d5ae645d92a307efa9d2c4b
Edu Montalvo <kaedu18@gmail.com>
2026-06-29 15:33:04 -0500

    feat: setup inicial con Vite, React, TypeScript y Tailwind v4

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-fondo min-h-screen text-texto">
+      <h1 className="text-lima text-2xl p-8">NovaTech 🚀</h1>
+    </div>
+  )
+}
+
+export default App
